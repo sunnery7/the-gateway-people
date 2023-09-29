c8f1c739a866fbd0c549b08813dcb8f861cf1459
sunnery7 <ayoadesunday97@gmail.com>
2023-09-29 13:37:43 +0100

    responsive header

--- components/header.tsx
+++ components/header.tsx
@@ -5,13 +5,13 @@ import { categories } from "../data"
 
 export default function Header(){
     return(
-        <header className="bg-white text-zinc-900 py-4 border-b" data-id="2">
+        <header className="bg-white text-zinc-900 py-4 border-b sticky top-0" data-id="2">
             <div className="container mx-auto px-4 md:px-6" data-id="3">
                 <nav className="flex items-center justify-between flex-col md:flex-row" data-id="4">
                     <div className="text-2xl font-bold" data-id="5"><Link href={`/`}>The Gateway People</Link></div>
-                    <div className="space-x-4" data-id="6">
+                    <div className="space-x-2 sm:space-x-4 pt-2" data-id="6">
                         {categories.map((category) => (
-                            <Link key={category.name} className="text-zinc-900 hover:text-zinc-700" data-id="7" href={`/category/${category.name}`}>{category.title}</Link>
+                            <Link key={category.name} className="text-zinc-900 hover:text-zinc-700 text-xs sm:text-base" data-id="7" href={`/category/${category.name}`}>{category.title}</Link>
                         ))}
                     </div>
                 </nav>
