b260792499ae74b4a542b8c3f62a4d678a2ed71f
sunnery7 <ayoadesunday97@gmail.com>
2023-10-01 18:26:51 +0100

    installed analytics and adsense

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-176268658-1" />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-176268658-1');`}
+        </Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9344411862068499" crossorigin="anonymous"></script>
+
+        <Script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        {/* <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async /> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
